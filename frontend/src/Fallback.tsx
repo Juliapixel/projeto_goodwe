38b7ed8549d4da4496f9518261a083445800a2c0
Julia <git@juliapixel.com>
2025-10-13 08:31:34 -0300

    fix(frontend): consertar pagina fallback

--- frontend/src/Fallback.tsx
+++ frontend/src/Fallback.tsx
@@ -1,15 +1,15 @@
 import { Link } from "react-router";
+import Button from "./components/Button";
 
 export default function Fallback() {
     return (
-        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col gap-2">
+        <div className="flex flex-col gap-2">
             <h1 className="text-center text-4xl font-bold">404</h1>
-            <p>Página não encontrada</p>
-            <Link
-                to="/"
-                className="block w-full py-2 text-center border border-zinc-300/30 bg-zinc-800 rounded-full"
-            >
-                Voltar
+            <p className="text-center">Página não encontrada</p>
+            <Link className="mx-auto" to="/">
+                <Button className="py-2 px-6">
+                    Voltar
+                </Button>
             </Link>
         </div>
     );
