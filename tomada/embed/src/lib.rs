97a68a03f3c31dd53c964b80ec66741691495fdf
Julia <git@juliapixel.com>
2025-10-14 17:29:56 -0300

    fix(tomada/embed): sincronizar led com relé
    fixes #16

--- tomada/embed/src/lib.rs
+++ tomada/embed/src/lib.rs
@@ -9,9 +9,9 @@
 
 use common::MessagePayload;
 #[cfg(not(feature = "ble"))]
-use embassy_futures::select::select5;
+use embassy_futures::select::select4;
 #[cfg(feature = "ble")]
-use embassy_futures::select::select6;
+use embassy_futures::select::select5;
 use embassy_sync::{blocking_mutex::raw::CriticalSectionRawMutex, signal::Signal, watch::Watch};
 use embassy_time::Timer;
 use esp_hal::gpio::{Input, InputConfig, InputPin, Level, Output, OutputConfig, OutputPin, Pull};
@@ -32,10 +32,8 @@ pub use fmt::*;
 pub use wifi::WifiHandler;
 
 pub struct App<'a> {
-    /// Dev board status LED
+    /// Status LED handler
     status_led: StatusLed<'a>,
-    /// Plug board status LED
-    plug_led: Output<'a>,
     /// Relay on plug
     relay: Output<'a>,
     /// Button on plug
@@ -54,19 +52,13 @@ impl<'a> App<'a> {
         wifi_handler: WifiHandler<'a>,
         #[cfg(feature = "ble")] ble_handler: BleHandler<'a>,
     ) -> Self {
-        let plug_led = Output::new(
-            plug_led_pin,
-            PlugLedMode::Off.into(),
-            OutputConfig::default(),
-        );
         let relay = Output::new(relay_pin, RelayMode::Closed.into(), OutputConfig::default());
         let button = Input::new(button_pin, InputConfig::default().with_pull(Pull::None));
 
-        let status_led = StatusLed::new(onboard_led_pin);
+        let status_led = StatusLed::new(onboard_led_pin, plug_led_pin);
 
         Self {
             status_led,
-            plug_led,
             relay,
             button,
             wifi: wifi_handler,
@@ -77,20 +69,18 @@ impl<'a> App<'a> {
 
     pub async fn run(#[allow(unused_mut)] mut self) -> ! {
         #[cfg(feature = "ble")]
-        select6(
+        select5(
             self.wifi.run(),
             self.ble.run(),
-            self.status_led.blink_led(),
-            led_task(&mut self.plug_led),
+            self.status_led.run(),
             relay_task(&mut self.relay),
             button_task(&mut self.button),
         )
         .await;
         #[cfg(not(feature = "ble"))]
-        select5(
+        select4(
             self.wifi.run(),
-            self.status_led.blink_led(),
-            led_task(&mut self.plug_led),
+            self.status_led.run(),
             relay_task(&mut self.relay),
             button_task(&mut self.button),
         )
@@ -128,6 +118,15 @@ impl From<Level> for RelayMode {
     }
 }
 
+impl RelayMode {
+    fn toggle(&self) -> Self {
+        match self {
+            RelayMode::Open => RelayMode::Closed,
+            RelayMode::Closed => RelayMode::Open,
+        }
+    }
+}
+
 pub static RELAY_SIGNAL: PinSignal<RelayMode> = Signal::new();
 pub static RELAY_STATUS: PinStatus<RelayMode> = Watch::new();
 
@@ -155,52 +154,6 @@ async fn relay_task(pin: &mut Output<'_>) {
     }
 }
 
-#[derive(Clone, Copy, PartialEq, Eq)]
-#[cfg_attr(feature = "defmt", derive(defmt::Format))]
-pub enum PlugLedMode {
-    On,
-    Off,
-}
-
-impl From<PlugLedMode> for Level {
-    fn from(value: PlugLedMode) -> Self {
-        match value {
-            PlugLedMode::On => Level::Low,
-            PlugLedMode::Off => Level::High,
-        }
-    }
-}
-
-impl From<Level> for PlugLedMode {
-    fn from(value: Level) -> Self {
-        match value {
-            Level::Low => PlugLedMode::On,
-            Level::High => PlugLedMode::Off,
-        }
-    }
-}
-
-pub static PLUG_LED_SIGNAL: PinSignal<PlugLedMode> = Signal::new();
-pub static PLUG_LED_STATUS: PinStatus<PlugLedMode> = Watch::new();
-
-async fn led_task(pin: &mut Output<'_>) {
-    let orig_level = pin.output_level();
-    let should_signal = PLUG_LED_STATUS
-        .try_get()
-        .is_none_or(|l| Level::from(l) == orig_level);
-    let sender = PLUG_LED_STATUS.sender();
-    if should_signal {
-        sender.send(orig_level.into());
-    }
-    loop {
-        let mode = PLUG_LED_SIGNAL.wait().await;
-        if mode != pin.output_level().into() {
-            pin.set_level(mode.into());
-            sender.send(mode);
-        }
-    }
-}
-
 pub static BUTTON_STATUS: PinStatus<ButtonEvent> = Watch::new();
 
 #[derive(Clone, Copy)]
@@ -210,6 +163,7 @@ pub enum ButtonEvent {
     Release,
 }
 
+/// Pin Level when plug button is pressed
 const BUTTON_PRESSED_LEVEL: Level = Level::Low;
 
 async fn button_task(pin: &mut Input<'_>) {
@@ -217,18 +171,14 @@ async fn button_task(pin: &mut Input<'_>) {
     let mut prev_level = pin.level();
     loop {
         pin.wait_for_any_edge().await;
+        // debounce de pobre
         Timer::after_millis(50).await;
         let level = pin.level();
         if level == prev_level {
             continue;
         } else if level == BUTTON_PRESSED_LEVEL {
             sender.send(ButtonEvent::Press);
-            let (rmode, lmode) = match RELAY_STATUS.try_get().unwrap_or(RelayMode::Open) {
-                RelayMode::Open => (RelayMode::Closed, PlugLedMode::On),
-                RelayMode::Closed => (RelayMode::Open, PlugLedMode::Off),
-            };
-            RELAY_SIGNAL.signal(rmode);
-            PLUG_LED_SIGNAL.signal(lmode);
+            RELAY_SIGNAL.signal(RELAY_STATUS.try_get().unwrap_or(RelayMode::Closed).toggle());
         } else {
             sender.send(ButtonEvent::Release);
         }
